1823d60c55017106214e7feb80fd399f698d8456
mamiline6 <ohmamirineoo6@gmail.com>
2015-07-20 02:51:49 +0900

    gulpfile の整理
    #21用のタスク追加
    ファイルの生成元・先変更

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,18 @@
-var gulp      = require('gulp');
-var sass      = require('gulp-sass');
-var rename = require('gulp-rename');
-var minify = require('gulp-minify-css');
+// gulp packagemodule
+var gulp         = require('gulp');
+var sass         = require('gulp-sass');
+var rename       = require('gulp-rename');
+var minify       = require('gulp-minify-css');
 var autoprefixer = require('gulp-autoprefixer');
+var plumber      = require('gulp-plumber');　// watch の解除をしない命令文
 
-// watch の解除をしない命令文
-var plumber = require('gulp-plumber');
-
+/* ====================
+    scss basic 
+ ==================== */
+// scss file
 var sassSrc = '_scss/**/*.scss';
 
+// task
 gulp.task('sass', function(){
   gulp.src(sassSrc)
     .pipe(plumber())
@@ -24,7 +28,32 @@ gulp.task('sass', function(){
     .pipe(gulp.dest('css'))
 });
 
+// ファイルを監視して、sass タスクを実行
 gulp.task('watch', function(){
-  // ファイルを監視して、sass タスクを実行
   gulp.watch(sassSrc, ['sass']);
+});
+
+/* ====================
+    scss tab #21
+ ==================== */
+// scss file
+var sassTabSrc = 'practice_tab/src/scss/**/*.scss';
+
+// css file
+var cssTabSrc = 'practice_tab/app/css';
+
+// task tab
+gulp.task('sasstab', function(){
+  gulp.src(sassTabSrc)
+    .pipe(plumber())
+    .pipe(sass({
+      style      : 'expanded'
+     }))
+    .pipe(autoprefixer(["last 2 version", "ie 8"]))
+    .pipe(gulp.dest(cssTabSrc))
+    // .pipe(minify())
+    .pipe(rename({
+      extname: '.min.css'
+    }))
+    .pipe(gulp.dest(cssTabSrc))
 });
