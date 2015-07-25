3e0858bf6024d1b1e6dd41dc90dfa6bd34de920f
mamiline6 <ohmamirineoo6@gmail.com>
2015-07-25 16:04:39 +0900

    インデント変更
    半角2スペースをタブに変更
    minify()のコメントアウト削除

--- gulpfile.js
+++ gulpfile.js
@@ -7,34 +7,34 @@ var autoprefixer = require('gulp-autoprefixer');
 var plumber      = require('gulp-plumber'); // watch の解除をしない命令文
 
 /* ====================
-    scss basic 
+	scss basic 
  ==================== */
 // scss file
 var sassSrc = '_scss/**/*.scss';
 
 // task
 gulp.task('sass', function(){
-  gulp.src(sassSrc)
-    .pipe(plumber())
-    .pipe(sass({
-      style      : 'expanded'
-     }))
-    .pipe(autoprefixer(["last 2 version", "ie 8"]))
-    .pipe(gulp.dest('css'))
-    .pipe(minify())
-    .pipe(rename({
-      extname: '.min.css'
-    }))
-    .pipe(gulp.dest('css'))
+	gulp.src(sassSrc)
+		.pipe(plumber())
+		.pipe(sass({
+			style : 'expanded'
+		}))
+		.pipe(autoprefixer(["last 2 version", "ie 8"]))
+		.pipe(gulp.dest('css'))
+		.pipe(minify())
+		.pipe(rename({
+			extname: '.min.css'
+		}))
+		.pipe(gulp.dest('css'))
 });
 
 // ファイルを監視して、sass タスクを実行
 gulp.task('watch', function(){
-  gulp.watch(sassSrc, ['sass']);
+	gulp.watch(sassSrc, ['sass']);
 });
 
 /* ====================
-    scss tab #21
+	scss tab #21
  ==================== */
 // scss file
 var sassTabSrc = 'practice_tab/src/scss/**/*.scss';
@@ -44,16 +44,16 @@ var cssTabSrc = 'practice_tab/app/css';
 
 // task tab
 gulp.task('sasstab', function(){
-  gulp.src(sassTabSrc)
-    .pipe(plumber())
-    .pipe(sass({
-      style      : 'expanded'
-     }))
-    .pipe(autoprefixer(["last 2 version", "ie 8"]))
-    .pipe(gulp.dest(cssTabSrc))
-    // .pipe(minify())
-    .pipe(rename({
-      extname: '.min.css'
-    }))
-    .pipe(gulp.dest(cssTabSrc))
+	gulp.src(sassTabSrc)
+		.pipe(plumber())
+		.pipe(sass({
+			outputStyle :'expanded'
+		}))
+		.pipe(autoprefixer(["last 2 version", "ie 8"]))
+		.pipe(gulp.dest(cssTabSrc))
+		.pipe(minify())
+		.pipe(rename({
+			extname: '.min.css'
+		}))
+		.pipe(gulp.dest(cssTabSrc))
 });
