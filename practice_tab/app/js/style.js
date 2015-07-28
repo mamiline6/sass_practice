44bf1b747cfae53f0bdda218bbbfd11c85b93070
mamiline6 <ohmamirineoo6@gmail.com>
2015-07-29 01:38:03 +0900

    style.js コピペ
    .eachの理解

--- practice_tab/app/js/style.js
+++ practice_tab/app/js/style.js
@@ -1,14 +1,29 @@
-$(function() {
-	// tab
-	var tabLink = $("#tabTopBlock .tabs__tab .tabs__link");
-	var tabCnt = $("#tabTopBlock .tabContents");
-	var tabItem = $("#tabTopBlock .tabs__tab");
+$(function(){
+//必要なセレクタ
+var selector = {
+	group: '.tab_block',
+	nav: '.tab__nav',
+	content: '.tab__content'
+};
+//選択時のクラス名
+var className = {
+	isSelected : 'is-selected'
+};
 
-	$(tabLink).click(function() {
-		var num = tabLink.index(this);
-		tabCnt.addClass("is-hidden");
-		tabCnt.eq(num).removeClass("is-hidden");
-		tabItem.removeClass("is-selected");
-		tabItem.eq(num).addClass("is-selected");
+	$(selector.group).each(function(idx, tab){
+		var $group = $(this);
+		var $navs = $group.find(selector.nav);
+		var $contents = $group.find(selector.content);
+
+		$navs.on('click', function(){
+			var $shown_nav = $(this);
+			var nav_idx = $shown_nav.index();
+			var $shown_content = $contents.eq(nav_idx);
+
+			$navs.removeClass(className.isSelected);
+			$shown_nav.addClass(className.isSelected);
+			$contents.removeClass(className.isSelected);
+			$shown_content.addClass(className.isSelected);
+		});
 	});
 });
